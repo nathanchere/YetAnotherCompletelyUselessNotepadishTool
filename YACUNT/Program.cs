dc93d8c5f6a8c9d83d7849897c656c5b2fe47798
Nathan Chere <nathan.chere@tretton37.com>
2015-01-22 20:12:02 +0100

    File open handler

--- YACUNT/Program.cs
+++ YACUNT/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -9,14 +10,29 @@ namespace YACUNT
     static class Program
     {        
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            
-            // TODO: Detect file type and launch appropriate viewer
 
+            string fileName = null;
+
+            if (args.Length == 0)
+            {
+                // Display file open dialog
+                // set fileName
+            }
+            else fileName = args[0];
+
+            if (string.IsNullOrEmpty(fileName)) return; // errors here
+            if (!File.Exists(fileName)) return; // errors here
+
+            var extension = Path.GetExtension(args[0]);
+
+            // TODO: Detect file type and launch appropriate viewer
             //Application.Run(new Form1());
+
+            MessageBox.Show("Opening format " + extension);
         }
     }
 }
--- YACUNT/Properties/AssemblyInfo.cs
+++ YACUNT/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("YACUNT")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Yet Another Completely Useless Notepad-ish Tool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nathan Chere")]
 [assembly: AssemblyProduct("YACUNT")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©2015 Nathan Chere")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
