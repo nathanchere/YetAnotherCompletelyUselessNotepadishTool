a0409ca6574f3776aa20bc365415e405ca0cd990
Nathan Chere <nathan.chere@tretton37.com>
2015-01-22 20:07:14 +0100

    Initial commit

---
+++ YACUNT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YACUNT
+{
+    static class Program
+    {        
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            // TODO: Detect file type and launch appropriate viewer
+
+            //Application.Run(new Form1());
+        }
+    }
+}
