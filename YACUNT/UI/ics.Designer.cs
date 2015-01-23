44096b69b7021574be17e44c85f1013f5acea92a
Nathan Chere <nathan.chere@tretton37.com>
2015-01-23 15:39:48 +0100

    ICS support beginning

---
+++ YACUNT/UI/ics.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace YACUNT.UI
+{
+    partial class ics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(774, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            this.menuStrip.Visible = false;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // ics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 359);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "ics";
+            this.Text = "ics";
+            this.Load += new System.EventHandler(this.ics_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ics_KeyDown);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
---
+++ YACUNT/UI/ics.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Windows.Forms;
+using iCal;
+
+namespace YACUNT.UI
+{
+    public partial class ics : Form
+    {
+        private readonly iCalendar _calendar;
+        private readonly string _fileName;
+
+        public ics(string filename)
+        {
+            InitializeComponent();
+
+            _fileName = filename;
+            Open();
+        }
+
+        private void Open()
+        {           
+            var _calendar = iCalendar.LoadFromFile(_fileName);
+
+        }
+
+        private void ics_Load(object sender, System.EventArgs e)
+        {
+            menuStrip.LostFocus += (o, args) => menuStrip.Visible = false;
+        }
+
+        private void ics_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Alt)
+            {
+                var isVisible = !menuStrip.Visible;
+                menuStrip.Visible = isVisible;
+                if (isVisible) menuStrip.Focus();
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                menuStrip.Visible = false;
+            }
+        }        
+    }
+}
